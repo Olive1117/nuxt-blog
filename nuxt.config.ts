89ae7dbe5542c85cd1a6829f98a6f302b9fb784e
Olive1117 <2657774950@qq.com>
2026-07-16 14:03:46 +0800

    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   $production: {
+    devtools: { enabled: false },
     vite: {
       esbuild: {
         drop: ['debugger', 'console'],
